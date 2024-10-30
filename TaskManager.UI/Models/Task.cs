eae4a827509567f3323e4e6d0134f5389a5003da
Raviv <raken100@gmail.com>
2024-10-30 22:43:24 +0200

    Improved the properties for the Task model

--- TaskManager.UI/Models/Task.cs
+++ TaskManager.UI/Models/Task.cs
@@ -5,6 +5,7 @@ namespace TaskManager.UI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
+        public bool IsComplete { get; set; }
+        public int Priority { get; set; }
     }
 }
