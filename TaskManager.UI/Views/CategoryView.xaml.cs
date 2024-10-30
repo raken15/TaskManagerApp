511e9e36f4e79cd1b8457e90732c8a1bc4126a2a
Raviv <raken100@gmail.com>
2024-10-30 20:07:14 +0200

    Created the basic view files and made the App.xaml to call the MainView when the application starts

---
+++ TaskManager.UI/Views/CategoryView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TaskManager.UI.Views
+{
+    public partial class CategoryView : Window
+    {
+        public CategoryView()
+        {
+            InitializeComponent(); // Links to the XAML file and initializes components
+        }
+    }
+}
---
+++ TaskManager.UI/Views/TaskView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TaskManager.UI.Views
+{
+    public partial class TaskView : Window
+    {
+        public TaskView()
+        {
+            InitializeComponent(); // Links to the XAML file and initializes components
+        }
+    }
+}
