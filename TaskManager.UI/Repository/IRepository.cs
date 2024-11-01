a3e4e2e17024c9cbbd730ec7e4cf622b7f414db1
Raviv <raken100@gmail.com>
2024-11-01 13:47:29 +0200

    updated the order of the methods declarations in the interface

--- TaskManager.UI/Repository/IRepository.cs
+++ TaskManager.UI/Repository/IRepository.cs
@@ -3,9 +3,9 @@ namespace TaskManager.UI.Repository
     public interface IRepository<T>
     {
         void Add(T entity);
-        void Delete(T entity);
         T FindById(int id);
         IEnumerable<T> GetAll();
         void Update(T entity);
+        void Delete(T entity);
     }
 }
